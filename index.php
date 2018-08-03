144b4460ebb5e9704060713c5449a123664b88b5
Daniel He <s3601775@student.rmit.edu.au>
2018-08-03 20:49:29 +1000

    Removed need to press ctrl + scroll to zoom in map
    Map now zooms without the need to hold control

--- index.php
+++ index.php
@@ -65,7 +65,8 @@
          function initMap() {
            map = new google.maps.Map(document.getElementById('map'), {
              center: {lat: -34.397, lng: 150.644},
-             zoom: 17
+             zoom: 17,
+             gestureHandling: 'greedy'
            });
            infoWindow = new google.maps.InfoWindow;
 
