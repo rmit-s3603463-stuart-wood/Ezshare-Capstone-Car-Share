aef95caf4760816092138a90f2158c2af7121057
Liam O'Brien <s3600748@student.rmit.edu.au>
2018-08-10 16:09:37 +1000

    fix moved nav php code on contacts and faqs

--- contacts.php
+++ contacts.php
@@ -1,20 +1,14 @@
 <html>
 	<head>
-	    <meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-		<link rel="stylesheet" type="text/css" href="/css/style.css">
-		<title>Contacts</title>
-		<?php
-			include_once('navbar.php');
-		?>
 		<?php
 			include_once('head.php');
 		?>
+		<title>Contacts</title>
 	</head>
 	<body>
+		<?php
+			include_once('navbar.php');
+		?>
 		<h1>Contact Us</h1>
 		<p>Have a question? Please get in touch by calling the number below.
 		 Our dedicated customer care team will get back to you as soon as possible. </p>
