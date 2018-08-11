c151f6fe11ad288eca87201c1ebd9f2a179c0d08
Daniel He <s3601775@student.rmit.edu.au>
2018-08-11 20:02:37 +1000

    Completed styling for payment page
     - Added form to input persoal data and credit card information
     - Added interactive credit card graphic

--- payment.php
+++ payment.php
@@ -1,80 +1,167 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <?php  include_once('head.php');  ?>
     <title>Payment</title>
-
+    <meta name="viewport" content="initial-scale=1">
      <!--  Bootstrap Code utilized is provided by w3schools at: https://www.w3schools.com/bootstrap4/
         Google Map code is provided by google developer documentation at: https://developers.google.com/maps/documentation/javascript/geolocation*/ -->
 
-    <link rel="stylesheet" href="css/creditly.css">
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script type="text/javascript" src="css/creditly.js"></script>
-    <script type="text/javascript">
-    $(function() {
-      var creditly = Creditly.initialize(
-          '.creditly-wrapper .expiration-month-and-year',
-          '.creditly-wrapper .credit-card-number',
-          '.creditly-wrapper .security-code',
-          '.creditly-wrapper .card-type');
+    <?php include_once('head.php'); ?>
 
-      $(".creditly-card-form .submit").click(function(e) {
-        e.preventDefault();
-        var output = creditly.validate();
-        if (output) {
-          // Your validated credit card output
-          console.log(output);
-        }
-      });
-    });
-  </script>
+    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+
+    
+
+    <link rel="stylesheet" href="css/card.css">
+
+    <link href="css/card-js.min.css" rel="stylesheet" type="text/css" />
+    <script src="css/card-js.min.js"></script>
+
+    <style type="text/css">
+    form button {
+      display: block;
+      margin-top: 15px;
+      width: 100%;
+      font-size: 12px;
+      padding: 8px 12px;
+    }
+  </style>
+    
 
   </head>
 
   <body>
 <?php  include_once('navbar.php');  ?>
 
-    <form class="creditly-card-form">
-    <section class="creditly-wrapper blue-theme">
-      <div class="credit-card-wrapper">
-        <div class="first-row form-group">
-          <div class="col-sm-8 controls">
-            <label class="control-label">Card Number</label>
-            <input class="number credit-card-number form-control"
-              type="text" name="number"
-              pattern="(\d*\s){3}\d*"
-              inputmode="numeric" autocomplete="cc-number" autocompletetype="cc-number" x-autocompletetype="cc-number"
-              placeholder="&#149;&#149;&#149;&#149; &#149;&#149;&#149;&#149; &#149;&#149;&#149;&#149; &#149;&#149;&#149;&#149;">
+<style>
+
+.demo-container {
+            width: 100%;
+            max-width: 350px;
+            margin: 5px auto;
+        }
+
+        form {
+            margin: 30px;
+        }
+        input {
+            width: 200px;
+            margin: 10px auto;
+            display: block;
+        }
+
+  var card = new Card({
+    // a selector or DOM element for the form where users will
+    // be entering their information
+    form: 'form', // *required*
+    // a selector or DOM element for the container
+    // where you want the card to appear
+    container: '.card-wrapper', // *required*
+
+    formSelectors: {
+        numberInput: 'input#number', // optional — default input[name="number"]
+        expiryInput: 'input#expiry', // optional — default input[name="expiry"]
+        cvcInput: 'input#cvc', // optional — default input[name="cvc"]
+        nameInput: 'input#name' // optional - defaults input[name="name"]
+    },
+
+    width: 200, // optional — default 350px
+    formatting: true, // optional - default true
+
+    // Strings for translation - optional
+    messages: {
+        validDate: 'valid\ndate', // optional - default 'valid\nthru'
+        monthYear: 'mm/yyyy', // optional - default 'month/year'
+    },
+
+    // Default placeholders for rendered fields - optional
+    placeholders: {
+        number: '•••• •••• •••• ••••',
+        name: 'Full Name',
+        expiry: '••/••',
+        cvc: '•••'
+    },
+
+    masks: {
+        cardNumber: '•' // optional - mask card number
+    },
+
+    // if true, will log helpful messages for setting up Card
+    debug: false // optional - default false
+});
+
+</style>
+    
+<div class="row">
+  <div class="col-75">
+    <div class="container">
+      <form action="/action_page.php">
+
+        <div class="row">
+          <div class="col-50">
+            <h3>Billing Address</h3>
+            <label for="fname"><i class="fa fa-user"></i> Full Name</label>
+            <input type="text" id="fname" name="firstname" placeholder="John Smith">
+            <label for="email"><i class="fa fa-envelope"></i> Email</label>
+            <input type="text" id="email" name="email" placeholder="john@example.com">
+            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
+            <input type="text" id="adr" name="address" placeholder="Example Street">
+            <label for="city"><i class="fa fa-institution"></i> City</label>
+            <input type="text" id="city" name="city" placeholder="Melbourne">
+
+            
+              
+                <label for="state">State</label>
+                <input type="text" id="state" name="state" placeholder="VIC">
+              
+              
+                <label for="zip">Zip</label>
+                <input type="text" id="zip" name="zip" placeholder="3000">
+              
+            
           </div>
-          <div class="col-sm-4 controls">
-            <label class="control-label">CVV</label>
-            <input class="security-code form-control"·
-              inputmode="numeric"
-              pattern="\d*"
-              type="text" name="security-code"
-              placeholder="&#149;&#149;&#149;">
-          </div>
+
+          <div class="col-50">
+            <h3>Payment</h3>
+            <label for="cname">Name on Card</label>
+            <input type="text" id="cname" name="name" placeholder="John Smith">
+            <label for="ccnum">Credit card number</label>
+            <input type="text" id="ccnum" name="number" placeholder="XXXX-XXXX-XXXX-XXXX">
+            <label for="expmonth">Expiry Date</label>
+            <input type="text" id="expmonth" name="expiry" placeholder="MM/YY">
+
+            <div class="row">
+              <div class="col-50">
+                <label for="cvv">CVV</label>
+                <input type="text" id="cvv" name="cvc" placeholder="XXX">
+              </div>
+            </div>
+            <div class="demo-container">
+          <div class="card-wrapper"></div>
         </div>
-        <div class="second-row form-group">
-          <div class="col-sm-8 controls">
-            <label class="control-label">Name on Card</label>
-            <input class="billing-address-name form-control"
-              type="text" name="name"
-              placeholder="John Smith">
           </div>
-          <div class="col-sm-4 controls">
-            <label class="control-label">Expiration</label>
-            <input class="expiration-month-and-year form-control"
-              type="text" name="expiration-month-and-year"
-              placeholder="MM / YY">
           </div>
-        </div>
-        <div class="card-type">
-        </div>
-      </div>
-    </section>
-    <button class="submit"><span>Submit</span></button>
-  </form>
+
+          
+          
+        <input type="submit" value="Continue to checkout" class="btn">
+      </form>
+    </div>
+  </div>
+
+</div>
+
+
+
+<script src="dist/card.js"></script>
+    <script>
+        new Card({
+            form: document.querySelector('form'),
+            container: '.card-wrapper'
+        });
+    </script>
+
   </body>
   <?php include_once('footer.php');?>
 
