ee411b79147132e0d50663eadfe51360e348cc50
Daniel He <s3601775@student.rmit.edu.au>
2018-08-11 23:41:37 +1000

    Added another car, added a button which will be used to find the closest car, started on a function to locate the nearest car
    - Added a red car at Melbourne airport
    - Added a button on map screen (top right) which will later be used to locate the nearest car
    - Started writing a function to locate the nearest car
    - Code refactoring

--- head.php
+++ head.php
@@ -19,3 +19,4 @@
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
