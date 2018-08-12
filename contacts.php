a4c74b3e119b561c0a6757ef52abf1333887a9c3
Liam O'Brien <s3600748@student.rmit.edu.au>
2018-08-12 19:34:31 +1000

    added content for the faqs and contact pages
    added a faqAccodian for the facts page for answering qustions with four questions benig added so far.
    added text improvements to the contacts page.
    Also added some general code touchups to both pages.

--- contacts.php
+++ contacts.php
@@ -1,21 +1,27 @@
-<html>
+<!doctype html>
+<html lang="en">
 	<head>
 		<?php include_once('head.php'); ?>
 		<title>Contacts</title>
 	</head>
 	<body>
-		<?php include_once('navbar.php'); ?>
-		<h1>Contact Us</h1>
-		<p>Have a question? Please get in touch by calling the number below.
-		 Our dedicated customer care team will get back to you as soon as possible. </p>
-		 <p>Customer care operating hours</p>
-		 <p>Monday - Friday: 9:00 AM - 5:30PM</p>
-		 <p>Saturday: 11:00 AM - 4:30PM</p>
-		 <p>Sundays: Closed</p>
-		 <P>Phone: 1800 145 234</P>
-		 <p>Location</p>
-		 <p>123EZshareHQ</p>
-		 <p>East Coburg VIC 3021</p>
+		<div class="container">
+			<?php include_once('navbar.php'); ?>
+			<h1 class = "text-center">Contact US</h1>
+			<p>Have a question? Please get in touch by calling the number below.</p>
+			<p>Our dedicated customer care team will get back to you as soon as possible.</p>
+			<p><mark><u>Customer care operating hours</u></mark></p>
+			<p><strong>Monday - Friday:</strong> 9:00 AM - 5:30PM</p>
+			<p><strong>Saturday:</strong> 11:00 AM - 4:30PM</p>
+			<p><strong>Sundays:</strong> Closed</p>
+			<P><mark><u>Phone:</u></mark> 1800 145 234</P>
+			<p><mark><u>Location</u></mark></p>
+			<p>123EZshareHQ</p>
+			<p>East Coburg VIC 3021</p>
+		 </div>
+		<div class="jumbotron text-center" style="margin-bottom:0">
+			<p>Footer</p>
+		</div>
 	</body>
 		<?php include_once('footer.php'); ?>
 </html>
