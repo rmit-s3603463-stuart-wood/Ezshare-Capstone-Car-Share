e643b3e2274e3c056f943e4f5d32a2c6c045fc53
rmit-s3603463-stuart-wood <s3603463@student.rmit.edu.au>
2018-08-12 21:15:20 +1000

    Added temporary input validation

--- bookinginfo.php
+++ bookinginfo.php
@@ -1,14 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="/css/style.css">
-
+      <?php  include_once('head.php');  ?>
     <title>EZshare - Car Hire on the Go</title>
 
      <!--  Bootstrap Code utilized is provided by w3schools at: https://www.w3schools.com/bootstrap4/
@@ -37,7 +30,7 @@
       float: left;
       clear: left;
       width: 250px;
-      text-align: right;
+      text-align: left;
     }
     input {
   display: inline-block;
@@ -46,53 +39,38 @@
 h1 {
         text-align: center;
 }
+
+div.b {
+  text-align: right;
+}
+
+input[type=number]{
+    width: 240px;
+}
        </style>
 
   </head>
   <body>
-    <!-- Optional JavaScript -->
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-      <a class="navbar-brand" href="#">EZshare</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="collapsibleNavbar">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Login\SignUp</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="booking.php">Make a Booking</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">FAQS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact Us</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+  <?php  include_once('navbar.php');  ?>
+
     <div id="info">
     <h1>Booking Information<h1>
       <hr>
       <h2>Car Description</h2>
       <hr>
       <h3> Car Model (Placeholder)</h3>
+      <h4> Car Licence Plate Number: 123ABC </h4>
       <h4> Cost per hour placeholder ($150) </h4>
       <br>
       <div class="formdiv">
       <form action="welcome.php" method="post">
-      <h4> <label> First Name:</label>  <input type="text" name="fname" placeholder="First name"><br>
-        <label>Last Name:</label>  <input type="text" name="lname" placeholder="Last Name"><br>
+      <h4> <label> First Name:</label>  <input type="text" name="fname" placeholder="First name" pattern="[A-Za-z]"><br>
+        <label>Last Name:</label>  <input type="text" name="lname" placeholder="Last Name" pattern="[A-Za-z]" ><br>
       <label>  E-mail:</label>  <input type="text" name="email"placeholder="Email"><br>
       <label>  Pickup Time: </label> <input type="time" name="ptime" placeholder="09/01/2018" ><br>
-      <label>  Credit Card Number:  </label> <input type="text" name="creditno" placeholder="Credit Card No."><br>
-      <label>  CVV </label> <input type="text" name="CVV" placeholder="CVV"> <br>
-      <label>  Drivers Licence Number: </label> <input type="text" name="name" placeholder="Driver Licence Number"><br>
+      <label>  Credit Card Number:  </label> <input type="number" name="creditno" placeholder="Credit Card No." min="16" max="16"><br>
+      <label>  CVV </label> <input type="number" name="CVV" placeholder="CVV" min="3" max="4"> <br>
+      <label>  Drivers Licence Number: </label> <input type="number" name="name" placeholder="Driver Licence Number" min="9" max="9"><br>
       <label>  Credit Card Expiry Date </label> <input type="date" name="ccdate" placeholder="09/01/2018"><br>
       <label><input type="submit" value="Book"/></label>
     </h4>
@@ -100,4 +78,7 @@ h1 {
           </div>
 </div>
   </body>
+  <?php  include_once('footer.php');  ?>
        <script>
+       </script>
+       </html>
---
+++ carInfo.php
@@ -0,0 +1,209 @@
+<!doctype html>
+<html lang="en">
+    <?php  include_once('head.php');  ?>
+        <title>EZshare - Car  Information</title>
+
+
+     <!--  Bootstrap Code utilized is provided by w3schools at: https://www.w3schools.com/bootstrap4/ -->
+     <!-- Pictures provided by:
+   https://www.carsales.com.au/dealer/details/Nissan-Pulsar-2016/OAG-AD-16152591/?gts=OAG-AD-16152591&gtssaleid=OAG-AD-16152591&rankingType=Showcase
+https://www.carsales.com.au/demo/details/Nissan-Patrol-2018/OAG-AD-15431504/?gts=OAG-AD-15431504&gtssaleid=OAG-AD-15431504&rankingType=Showcase
+https://www.carsales.com.au/bncis/details/Nissan-GT-R-2017/OAG-AD-16064743/?Cr=5
+ -->
+
+  </head>
+  <body>
+    <?php  include_once('navbar.php');  ?>
+
+    <div class="container table-responsive-sm">
+      <h1 class = "text-center"> Our Fleet</h1>
+
+      <ul class="nav nav-tabs" role="tablist">
+        <li class="nav-item">
+          <a class="nav-link active" data-toggle="tab" href="#Tier1">Tier 1</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="tab" href="#Tier2">Tier 2</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="tab" href="#Tier3">Tier 3</a>
+        </li>
+      </ul>
+<div class="tab-content">
+
+  <div id="Tier1" class="container tab-pane active"><br>
+
+      <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <th colspan = "3">Tier 1</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td rowspan="8">  <img src="resources\assets\icons\sedan.jpg"  class="rounded img-fluid"  alt="sedan" width="600" height="500"> </td>
+
+            <th>Registration Number:</th>
+            <td>SED123</td>
+
+          </tr>
+          <tr>
+            <th>Model:</th>
+            <td>Pulsar ST B17 Series</td>
+
+          </tr>
+          <tr>
+            <th>Make:</th>
+            <td>Nissan</td>
+
+          </tr>
+          <tr>
+            <th>Year:</th>
+            <td>2016</td>
+
+          </tr>
+          <tr>
+            <th>Tier:</th>
+            <td>1</td>
+
+          </tr>
+          <tr>
+            <th>Number of Seats:</th>
+            <td>5</td>
+
+          </tr>
+          <tr>
+            <th>Engine Type:</th>
+            <td> 4 cylinder Petrol Aspirated 1.8L </td>
+
+          </tr>
+          <tr>
+            <th>Rental Price per hour:</th>
+            <td>$35</td>
+
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div id="Tier2" class="container tab-pane fade"><br>
+
+      <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <th colspan = "3">Tier 2</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td rowspan="8"><img src="resources\assets\icons\4wd.jpg"  class="rounded img-fluid"  alt="4wd" width="600" height="500"></td>
+
+            <th>Registration Number:</th>
+            <td>BIG123</td>
+
+          </tr>
+          <tr>
+            <th>Model:</th>
+            <td>Patrol Ti-L Y62 Series 4</td>
+
+          </tr>
+          <tr>
+            <th>Make:</th>
+            <td>Nissan</td>
+
+          </tr>
+          <tr>
+            <th>Year:</th>
+            <td>2018</td>
+
+          </tr>
+          <tr>
+            <th>Tier:</th>
+            <td>2</td>
+
+          </tr>
+          <tr>
+            <th>Number of Seats:</th>
+            <td>7</td>
+
+          </tr>
+          <tr>
+            <th>Engine Type:</th>
+            <td>8 cylinder Petrol Aspirated 5.6L</td>
+
+          </tr>
+          <tr>
+            <th>Rental Price per hour:</th>
+            <td>$50</td>
+
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div id="Tier3" class="container tab-pane fade"><br>
+
+      <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <th colspan = "3">Tier 3</th>
+          </tr>
+        </thead>
+        <tbody>
+
+
+          <tr>
+            <td rowspan="8"><img src="resources\assets\icons\luxury.jpg"  class="rounded img-fluid"  alt="luxury" width="600" height="500"></td>
+
+            <th>Registration Number:</th>
+            <td>LUX123</td>
+
+          </tr>
+          <tr>
+            <th>Model:</th>
+            <td>GT-R NISMO R35</td>
+
+          </tr>
+          <tr>
+            <th>Make:</th>
+            <td>Nissan</td>
+
+          </tr>
+          <tr>
+            <th>Year:</th>
+            <td>2017</td>
+
+          </tr>
+          <tr>
+            <th>Tier:</th>
+            <td>3</td>
+
+          </tr>
+          <tr>
+            <th>Number of Seats:</th>
+            <td>4</td>
+
+          </tr>
+          <tr>
+            <th>Engine Type:</th>
+            <td>6 cylinder Petrol Twin Turbo Intercooled 3.8L</td>
+
+          </tr>
+          <tr>
+            <th>Rental Price per hour:</th>
+            <td>$75</td>
+
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    </div>
+
+    </div>
+
+    <div class="jumbotron text-center" style="margin-bottom:0">
+      <p>Footer</p>
+    </div>
+
+
+    </body>
+    <?php  include_once('footer.php');?>
+</html>
