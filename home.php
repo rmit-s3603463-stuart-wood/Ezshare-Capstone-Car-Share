4f6621dd50cebdab6cbae58b188503f8718d463e
Liam O'Brien <s3600748@student.rmit.edu.au>
2018-08-14 20:11:28 +1000

    template pages
    created template for new home page and created css files

---
+++ home.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<?php include_once('head.php'); ?>
+	<link rel="stylesheet" type="text/css" href="cssHome.css">
+	<title>Home</title>
+</head>
+<body>
+	<?php include_once('navbar.php'); ?>
+	<div class="container">
+	</div>
+</body>
+<?php include_once('footer.php'); ?>
+</html>
