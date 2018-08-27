2587960a220f613a1a78e276cd244730f7b7a433
rmit-s3588936-Chris-Joseph-Constantinou <s3588936@student.rmit.edu.au>
2018-08-27 15:49:05 +1000

    log in page edits
    changed landing page for logged in user

--- signUpPro.php
+++ signUpPro.php
@@ -104,7 +104,7 @@ if (isset($_POST['login_user'])) {
   	if (mysqli_num_rows($results) == 1) {
   	  $_SESSION['email'] = $email;
   	  $_SESSION['success'] = "You are now logged in";
-  	  header('location: index.php');
+  	  header('location: myAccount.php');
   	}else {
   		array_push($errors, "Wrong username/password combination");
   	}
