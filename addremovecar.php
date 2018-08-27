13ca83ac5044370444d1955e78c82602e766d58e
rmit-s3603463-stuart-wood <s3603463@student.rmit.edu.au>
2018-08-27 15:20:52 +1000

    Update addremovecar.php

--- addremovecar.php
+++ addremovecar.php
@@ -32,7 +32,6 @@
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://www.paypalobjects.com/api/checkout.js"></script>
 
 
   </head>
