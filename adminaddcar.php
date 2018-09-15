c2912d361fa49503aa0e60a8931d524c1538aa16
Liam O'Brien <s3600748@student.rmit.edu.au>
2018-09-15 20:06:12 +1000

    stuff
    bunch of little features added to admin pages

--- adminaddcar.php
+++ adminaddcar.php
@@ -1,5 +1,22 @@
 <?php include('head.php');
 
+$rego = "";
+$model = "";
+$make ="";
+$year ="";
+$tier ="";
+$seatNo ="";
+$engine ="";
+$price ="";
+$stationName ="";
+$carCords ="";
+$booked ="";
+$availability ="";
+$totalkm ="";
+$journeykm ="";
+$currDriver ="";
+$errors = array();
+
 // Add Car
 if (isset($_POST['submit'])) {
   // receive all input values from the form
@@ -23,8 +40,23 @@ if($stationName = 'RMIT')
 	$carCords = '-37.885222, 145.086158';
   }
   $booked = true;
-  $availability = true; 
+  $availability = true;
+  $totalkm = $_POST['totalkm'];
+  $journeykm = '0';
+  $currDriver = Null;
 
+  if (empty($rego)) { array_push($errors, "rego is required"); }
+  if (empty($model)) { array_push($errors, "car model is required"); }
+  if (empty($make)) { array_push($errors, "car make is required"); }
+  if (empty($year)) { array_push($errors, "year number is required"); }
+  if (empty($tier)) { array_push($errors, "car tier is required"); }
+  if (empty($seatNo)) { array_push($errors, "seatNo name is required"); }
+  if (empty($engine)) { array_push($errors, "engine is required"); }
+  if (empty($price)) { array_push($errors, "price is required"); }
+  if (empty($carPic)) { array_push($errors, "carPic is required"); }
+  if (empty($stationName)) { array_push($errors, "stationName is required"); }
+  if (empty($totalkm)){ array_push($errors, "total km/s for car is required"); }
+  
   // first check the database to make sure
   // a car does not already exist with the same rego
   $car_check_query = "SELECT * FROM cars WHERE  rego='$rego' LIMIT 1";
@@ -32,9 +64,11 @@ if($stationName = 'RMIT')
   $car = mysqli_fetch_assoc($result);
   if ($car) { // if user exists
     if ($car['rego'] === $rego) {
-      echo "rego is already being used! Try another";
+	array_push($errors, "rego is already being used! Try another");
     }
   }
+  
+  if (count($errors) == 0) {
 $servername = "localhost";
 $username = "Admin";
 $password = "password";
@@ -45,23 +79,23 @@ $conn = new mysqli($servername, $username, $password,$dbname);
 if ($conn->connect_error) {
    die("Database Connection failed: " . $conn->connect_error);
 }
-$query = "INSERT INTO cars (rego, model, make, year, tier, seatNo, engine, price, carPic, stationName, carCords, booked, availability)
-  			  VALUES ('$rego', '$model', '$make', '$year', '$tier', '$seatNo', '$engine', '$price', '$carPic', '$stationName', '$carCords', '$booked', '$availability')";
+$query = "INSERT INTO cars (rego, model, make, year, tier, seatNo, engine, price, carPic, stationName, carCords, booked, availability, totalkm, journeykm, currDriver)
+  			  VALUES ('$rego', '$model', '$make', '$year', '$tier', '$seatNo', '$engine', '$price', '$carPic', '$stationName', '$carCords', '$booked', '$availability', '$totalkm', '$journeykm', '$currDriver')";
 	
 			  mysqli_query($conn, $query);
 			  if(mysqli_query($conn, $query))
 			  {
-				echo"car has been added to the database";
+					if(move_uploaded_file($_FILES['carPic']['tmp_name'], $target))
+					{
+						echo '<script language="javascript">';
+						echo 'alert("car has been added to database")';
+						echo '</script>';
+					}
 			  }else{
-				  echo"car has not been added to the database";
+				echo '<script language="javascript">';
+				echo 'alert("car has been added to database")';
+				echo '</script>';
 			  }
-			  
-	if(move_uploaded_file($_FILES['carPic']['tmp_name'], $target))
-	{
-		echo"image uploaded successfully";
-	}else{
-		die("Error: {$conn->errno} : {$conn->error}");
-		echo"your image has not been uploaded!";
 	}
 }
   ?>
@@ -73,15 +107,13 @@ $query = "INSERT INTO cars (rego, model, make, year, tier, seatNo, engine, price
       Google Map code is provided by google developer documentation at: https://developers.google.com/maps/documentation/javascript/geolocation*/ -->
       
       <link rel="stylesheet" href="css/card.css">
-      <link href="css/card-js.min.css" rel="stylesheet" type="text/css" />
-      <script src="css/card-js.min.js"></script>
   </head>
   <body>
     <?php  include_once('navbar.php');  ?>
     <div class="container">
         <h2>Add a Car</h2>
         <form method="post" enctype="multipart/form-data" action="adminaddcar.php" name="addcar" role="form">
-			
+			<?php include('errors.php'); ?>
             <label for="rego">Registration</label><input type="text" id="rego" name="rego" placeholder="Please provide registration">
             
 			<label for="model">Car Model</label><input type="text" id="model" name="model" placeholder="Please provide car model">
@@ -118,7 +150,9 @@ $query = "INSERT INTO cars (rego, model, make, year, tier, seatNo, engine, price
 															<option value="Chadstone">Chadstone</option>
 														  </select>
             
-			<button type="submit" class="btn" name="submit">Add Car</button>
+			<label for="totalkm">total km/s</label><input type="number" id="totalkm" name="totalkm" placeholder="Please provide total km/s of car">
+			
+			<button type="submit" class="btn" name="submit" onclick="return confirm('Are you sure you wish to add this car?')">Add Car</button>
         </form>
 	</div>
     </body>
